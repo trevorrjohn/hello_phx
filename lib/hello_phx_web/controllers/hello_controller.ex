835ed2a97888c7c9d9c04a21d39a04a9fe9ce310
Trevor John <trevorrjohn@gmail.com>
2025-02-10 11:17:17 -0500

    Plug for only specific controller actions

--- lib/hello_phx_web/controllers/hello_controller.ex
+++ lib/hello_phx_web/controllers/hello_controller.ex
@@ -1,6 +1,8 @@
 defmodule HelloPhxWeb.HelloController do
   use HelloPhxWeb, :controller
 
+  plug HelloWeb.Plugs.Locale, "en" when action in [:index]
+
   def index(conn, _params) do
     render(conn, :index)
   end
--- lib/hello_phx_web/router.ex
+++ lib/hello_phx_web/router.ex
@@ -8,7 +8,6 @@ defmodule HelloPhxWeb.Router do
     plug :put_root_layout, html: {HelloPhxWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-    plug HelloWeb.Plugs.Locale, "en"
   end
 
   pipeline :api do
