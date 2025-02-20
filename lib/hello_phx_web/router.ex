67f0c9dd745fa49510bf604e9ef200c494e7cbe7
Trevor John <trevorrjohn@gmail.com>
2025-02-20 16:11:45 -0500

    ShoppingCart first pass with session

--- lib/hello_phx_web/router.ex
+++ lib/hello_phx_web/router.ex
@@ -8,10 +8,36 @@ defmodule HelloPhxWeb.Router do
     plug :put_root_layout, html: {HelloPhxWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
+    plug :fetch_current_cart
   end
 
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  defp fetch_current_user(conn, _) do
+    if user_uuid = get_session(conn, :current_uuid) do
+      assign(conn, :current_uuid, user_uuid)
+    else
+      new_uuid = Ecto.UUID.generate()
+
+      conn
+      |> assign(:current_uuid, new_uuid)
+      |> put_session(:current_uuid, new_uuid)
+
+    end
+  end
+
+  alias HelloPhx.ShoppingCart
+
+  defp fetch_current_cart(conn, _) do
+    if cart = ShoppingCart.get_cart_by_user_uuid(conn.assigns.current_uuid) do
+      assign(conn, :cart, cart)
+    else
+      {:ok, cart} = ShoppingCart.create_cart(conn.assigns.current_uuid)
+      assign(conn, :cart, cart)
+    end
   end
 
   scope "/", HelloPhxWeb do
